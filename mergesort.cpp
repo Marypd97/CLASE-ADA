52deaa017d917b7ec881518aefcf6e38584c7b64
Marypd97 <mariangela.pacheco@ucsp.edu.pe>
2016-03-23 20:45:16 -0500

    Update mergesort.cpp

--- mergesort.cpp
+++ mergesort.cpp
@@ -16,15 +16,17 @@ void Merge(int *A,int ini,int med, int fin)
         L[i]=A[ini+i];
   }
 
-  for(j=0;j<n2;j++)
+  for(j=0;j<=n2;j++)
   {
         R[j]=A[med+j+1];
   }
 i=0;
 j=0;
-  for (int k=ini; k<fin;k++)
+L[n1]=1000000;
+R[n2]=1000000;
+  for (int k=ini; k<=fin;k++)
   {
-        if(L[i]<R[j])
+        if(L[i]<=R[j])
         {
             A[k]=L[i];
             i++;
@@ -34,7 +36,7 @@ j=0;
             A[k]=R[j];
             j++;
         }
- 
+
   }
 }
 void Merge_sort(int *A,int p, int r)
@@ -49,7 +51,31 @@ void Merge_sort(int *A,int p, int r)
         Merge(A,p,q,r);
     }
 }
+int main()
+{
+     cout << "***MERGE SORT***" << endl<<endl;
+     int A[6]={5,2,3,8,6,4};
+    int B[9]={9,5,3,15,8,19,5,4,13};
+    int *a;
 
+    a=A;
 
+    Merge_sort(a,0, 6);
+    cout << "LISTA A" << endl;
+    for(int i=0;i<6;i++)
+    {
+        cout<<A[i]<<", ";
+    }
 
-#endif // MERGE_H_INCLUDED
+    int *b;
+    b=B;
+
+    Merge_sort(b,0, 9);
+    cout <<endl<< "LISTA B" << endl;
+    for(int i=0;i<9;i++)
+    {
+        cout<<B[i]<<", ";
+    }
+
+    return 0;
+}
